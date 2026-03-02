9f982f229c1d33921ec7218f2d50583ff1025526
Nguyen Tieu Boi <boint@ACTAD.LOCAL>
2026-03-02 13:32:56 +0700

    feat(success): init project FE

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from '@/assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={reactLogo} alt="React Logo" />
+      <p>React Logo</p>
+    </>
+  )
+}
+
+export default App
