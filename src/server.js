3265dd09471b3997a4dd12a1f6ea7b8c00cbd948
Nguyễn Tiểu Bối <boint99@gmail.com>
2026-03-03 22:22:26 +0700

    feat(success): Complete the configuration and database connection process.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 import express from 'express'
-import dotenv from 'dotenv'
+import CONNECT_DB from './configs/config.db.js'
+import { environments } from './configs/environments.js'
 
-dotenv.config()
+const START_SERVER = async () => {
+  const app = express()
+  const port = environments.API_PORT || 8017
 
-const app = express()
-const port = process.env.BACKEND_PORT || 8017
+  // Fix from disk cache
+  app.use((req, res, next) => {
+    res.set('Cache-control', 'no-store')
+    next()
+  })
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the CRM project.')
-})
+  app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port}`)
-})
+  app.get('/', (req, res) => {
+    res.send('Welcome to the CRM project')
+  })
+
+  app.listen(port, () => {
+    console.log(`🚀 CRM Backend is running at http://localhost:${port}`)
+  })
+}
+(async () => {
+  try {
+    console.log('--- Khởi động tiến trình kết nối ---')
+    await CONNECT_DB()
+    console.log('✅ Connected to Postgres!')
+    await START_SERVER()
+  } catch (error) {
+    console.error('❌ Lỗi hệ thống:', error.message)
+    process.exit(1)
+  }
+})()
