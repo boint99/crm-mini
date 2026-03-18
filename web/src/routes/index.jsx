09bfa04420eab3c86af0952f1e92f5c36cbc91ee
Nguyen Tieu Boi <boint@ACTAD.LOCAL>
2026-03-18 11:53:00 +0700

    fix(bug): forder auth => Auth

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,32 +1,32 @@
-import MainLayout from '@/components/layouts/MainLayout'
+import MainLayout from "@/components/layouts/MainLayout";
 
-import Dashboard from '@/pages/Dashboard'
-import Employees from '@/pages/Employees'
-import NotFound from '@/pages/NotFound'
+import Dashboard from "@/pages/Dashboard";
+import Employees from "@/pages/Employees";
+import NotFound from "@/pages/NotFound";
 
-import Auth from '@/pages/Auth'
-import Login from '@/pages/auth/Login'
-import Register from '@/pages/auth/Register'
+import Auth from "@/pages/Auth";
+import Login from "@/pages/Auth/Login";
+import Register from "@/pages/Auth/Register";
 
 const routes = [
-    {
-        path: '/',
-        element: <MainLayout />,
-        children: [
-            { index: true, element: <Dashboard /> },
-            { path: 'employees', element: <Employees /> },
-            { path: '*', element: <NotFound /> },
-        ],
-    },
-    {
-        path: '/auth',
-        element: <Auth />,
-        children: [
-            { path: 'login', element: <Login /> },
-            { path: 'register', element: <Register /> },
-            { path: '*', element: <NotFound /> },
-        ],
-    },
-]
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "employees", element: <Employees /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+];
 
-export default routes
+export default routes;
