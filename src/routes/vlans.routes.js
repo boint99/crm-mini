f90c4df6a91d151248ebb1c2a95349ab182bc3c3
Nguyen Tieu Boi <boint@ACTAD.LOCAL>
2026-04-08 14:01:18 +0700

    bug: link api web vlan

--- src/routes/vlans.routes.js
+++ src/routes/vlans.routes.js
@@ -5,7 +5,7 @@ import { vlansValidate } from '../validates/vlans.validate.js'
 const Router = express.Router()
 
 // GET - /api/vlans/lists
-Router.get('/vlans/lists', vlansController.lists)
+Router.get('/vlan/lists', vlansController.lists)
 
 // // POST /api/vlans
 Router.post('/vlan/create', vlansValidate.create, vlansController.create)
