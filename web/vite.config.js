dfe33ae9a5e663838631e008e65767c2f0be33a5
Nguyen Tieu Boi <boint@ACTAD.LOCAL>
2026-07-20 13:40:55 +0700

    fix: file .env web

--- web/vite.config.js
+++ web/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    envDir: path.resolve(__dirname, '..'),
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src')
