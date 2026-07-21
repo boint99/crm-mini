6d89e14e09912038953f7827fc5a2a748c17ca67
Nguyen Tieu Boi <boint@ACTAD.LOCAL>
2026-07-21 11:25:10 +0700

    feat: add page hosting

---
+++ web/src/pages/Hosting/ACTES.jsx
@@ -0,0 +1,10 @@
+import IframePage from './IframePage'
+
+export default function ACTES() {
+  return (
+    <IframePage
+      title="ACTES"
+      url="https://cpanel.actes.vn"
+    />
+  )
+}
---
+++ web/src/pages/Hosting/VienThongACT.jsx
@@ -0,0 +1,10 @@
+import IframePage from './IframePage'
+
+export default function VienThongACT() {
+  return (
+    <IframePage
+      title="VienThongACT"
+      url="https://cpanel.vienthongact.vn"
+    />
+  )
+}
